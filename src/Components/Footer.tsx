be3a915cde8ebd5292a4a860fba2abfddc060d7d
@michelevitali2001 <michelevitali2001@gmail.com>
2025-11-16 09:17:45 +0100

    Primo commit dei file: impostazione generale del codice css

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+import "../App.css";
+function Footer (){
+    return(
+        <>
+            <div class="footer">
+            <p>Ciao al</p>
+            </div>
+        </>
+    )
+}export default Footer;
