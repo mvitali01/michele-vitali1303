93e380415cb87f198a7750b6256b78599a46876b
@michelevitali2001 <michelevitali2001@gmail.com>
2026-09-11 00:57:17 +0200

    Aggiunta icone nuove e sistemazione skills. Correzioni generali ed aggunte nel CSS.

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -5,13 +5,89 @@ function Skills() {
             <div className="box">  
                     <h1 className="skills-title">Skills</h1>
                     <br/>
-                    <img src="/images/js.png" width="50px" height="50px"/>
-                    &nbsp;&nbsp; &nbsp;&nbsp;
-                    <img src="/images/java.png" width="50px" height="50px"/>
-                    &nbsp;&nbsp; &nbsp;&nbsp;
-                    <img src="/images/nodejs.png" width="50px" height="50px"/>
-                    &nbsp;&nbsp; &nbsp;&nbsp;
-                    <img src="images/gnu-bash.png" width="50px" height="50px"/>
+                    <div className="box-skills">
+                        <div className="imgbox">
+                                <img src="/images/js.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">Javascript</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/html.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">HTML</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/css-3.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">CSS</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/react.svg" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">React</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/nodejs.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">Node.js</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/gnu-bash.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">Bash</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/c-programming.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg"> C </div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/c++.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">C++</div>
+                                </div>
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="box-skills">
+                        <div className="imgbox">
+                                <img src="/images/java.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">Java</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/python.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">Python</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/mysql.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">MySQL</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/mongoDB.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">MongoDB</div>
+                                </div>
+                        </div>
+                        <div className="imgbox">
+                                <img src="/images/c-sharp.png" className="skill-logo"/>
+                                <div className="infoimg">
+                                        <div className="textimg">C#</div>
+                                </div>
+                        </div>
+                    </div>
             </div>
         )
 }
